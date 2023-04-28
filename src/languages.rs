23637b64eac08785eb937b11045c9add7f4d792e
Enrico Zandomeni Borba <enricozb@gmail.com>
2023-04-28 13:49:09 -0400

    clean up languages.rs

--- src/languages.rs
+++ src/languages.rs
@@ -1,41 +1,10 @@
 use anyhow::{anyhow, Result};
 use tree_sitter::Language;
-use tree_sitter_highlight::HighlightConfiguration;
 
-static HIGHLIGHT_NAMES: [&str; 18] = [
-  "attribute",
-  "constant",
-  "comment",
-  "function",
-  "function.builtin",
-  "function.macro",
-  "keyword",
-  "operator",
-  "property",
-  "string",
-  "string.special",
-  "tag",
-  "type",
-  "type.builtin",
-  "variable",
-  "variable.builtin",
-  "variable.parameter",
-  "constructor",
-];
-
-/// Returns the `HighlightConfiguration` for a given language.
-pub fn highlighter_config(language: &str) -> Result<HighlightConfiguration> {
-  let (lang, hl_query, inj_query) = language_queries(language)?;
-  let mut config = HighlightConfiguration::new(lang, hl_query, inj_query, "")?;
-  config.configure(&HIGHLIGHT_NAMES);
-
-  Ok(config)
-}
-
-/// Returns the queries required to create a `HighlightConfiguration` for a given language.
-fn language_queries(language: &str) -> Result<(Language, &str, &str)> {
+/// Return a language given a kakoune filetype string.
+pub fn from(language: &str) -> Result<Language> {
   match language {
-    "rust" => Ok((tree_sitter_rust::language(), tree_sitter_rust::HIGHLIGHT_QUERY, "")),
-    _ => Err(anyhow!("unsupported language: {language}")),
+    "rust" => Ok(tree_sitter_rust::language()),
+    _ => Err(anyhow!("unknown language: {language}")),
   }
 }
--- src/tree.rs
+++ src/tree.rs
@@ -3,25 +3,24 @@ use std::{fs, path::Path};
 use anyhow::{anyhow, Result};
 use tree_sitter::{Parser as TSParser, Tree};
 
-pub struct Parser {
-  parser: TSParser,
-}
+use crate::languages;
+
+/// A wrapper to tree-sitter's [`TSParser`].
+pub struct Parser(TSParser);
 
 impl Parser {
   /// Creates a new `Parser`.
   pub fn new(language: &str) -> Result<Self> {
     let mut parser = TSParser::new();
-    parser.set_language(tree_sitter_rust::language())?;
+    parser.set_language(languages::from(language)?)?;
 
-    // TODO(enricozb): parser.set_language(Language::from(language))
-
-    Ok(Self { parser })
+    Ok(Self(parser))
   }
 
   /// Parses the file located at `content_file`.
   pub fn parse_file(&mut self, content_file: &Path) -> Result<Tree> {
     let content = fs::read(content_file)?;
 
-    self.parser.parse(content, None).ok_or(anyhow!("parsing error"))
+    self.0.parse(content, None).ok_or(anyhow!("parsing error"))
   }
 }
