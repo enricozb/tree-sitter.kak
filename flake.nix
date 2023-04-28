36d9080ab34b354e4448a214a132f13173be6509
Enrico Zandomeni Borba <enricozb@gmail.com>
2023-04-28 16:40:02 -0400

    add flake-compat

--- flake.nix
+++ flake.nix
@@ -3,6 +3,12 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
+  # flake compatability
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
